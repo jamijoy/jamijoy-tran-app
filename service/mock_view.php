26e7a7e91d96432b3fee3bdb864f8cf9a4a9cb0d
jami-at-871761521336 <al.jami@paperfly.com.bd>
2023-10-12 17:43:20 +0600

    mock response view and call

--- service/mock_view.php
+++ service/mock_view.php
@@ -1,1 +1,28 @@
 <?php
+
+$mockStatus = isset($_SERVER['HTTP_X_MOCK_STATUS']) ? $_SERVER['HTTP_X_MOCK_STATUS'] : "200";
+header('Content-Type: application/json');
+
+$responseData = [
+    "message" => "Mock response for transaction api",
+];
+
+if ($mockStatus === "200") {
+
+    http_response_code(200);
+    $responseData["status"] = "accepted";
+    $responseData["transaction_id"] = $_SERVER['HTTP_X_TRANSACTION_ID'];
+} elseif ($mockStatus === "400") {
+
+    http_response_code(400);
+    $responseData["status"] = "failed";
+    $responseData["error_message"] = "Bad Request";
+} else {
+
+    http_response_code(500);
+    $responseData["status"] = "error";
+    $responseData["error_message"] = "Internal Server Error";
+}
+
+
+echo json_encode($responseData);
--- service/store_transaction.php
+++ service/store_transaction.php
@@ -1,1 +1,28 @@
 <?php
+
+$mockApiUrl = "http://localhost:80/jamijoy-tran-app/transaction-mock";
+
+
+$headers = [
+    "X-Mock-Status: 200", // instial response
+    "X-Transaction-ID: 9001"
+];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $mockApiUrl);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+
+$response = curl_exec($ch);
+
+
+if (curl_errno($ch)) {
+    echo 'cURL error: ' . curl_error($ch);
+}
+
+
+curl_close($ch);
+
+
+echo $response;
